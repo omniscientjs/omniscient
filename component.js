cc57c482a28c0f72e928bde257075eb271d6e6d0
Mikael Brevik <mikaelbre@gmail.com>
2014-10-09 13:11:47 +0200

    Adds possibility of having mixins for components.

--- component.js
+++ component.js
@@ -1,10 +1,20 @@
 var React = require('react');
 
-module.exports = function component (fn) {
+module.exports = function component (mixins, render) {
+  if (typeof mixins === 'function') {
+    render = mixins;
+    mixins = [];
+  }
+
+  if (!Array.isArray(mixins)) {
+    mixins = [mixins];
+  }
+
   var Component = React.createClass({
+    mixins: mixins,
 
     render: function () {
-      return fn.call(this, this.props.cursor, this.props.statics);
+      return render.call(this, this.props.cursor, this.props.statics);
     },
 
     shouldComponentUpdate: function (nextProps) {
