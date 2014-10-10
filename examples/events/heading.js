f20770c51d96abcefb059a270d3ab9cdd0cb0c81
Mikael Brevik <mikaelbre@gmail.com>
2014-10-10 22:07:13 +0200

    Moves all examples to homepage for hosting

--- examples/events/heading.js
+++
@@ -1,9 +0,0 @@
-var React = require('react'),
-    component = require('omniscient');
-
-module.exports = component(function (data, statics) {
-  function onClick () {
-    statics.events.emit('event', new Date());
-  }
-  return React.DOM.text({ onClick: onClick }, data.text);
-});
--- examples/index.js
+++
@@ -1,4 +0,0 @@
-DemoEvents     = require('./events');
-DemoInlineEdit = require('./inline-edit');
-DemoRRouter    = require('./rrouter');
-DemoEntryList  = require('./entry-list');
