d281a32a16ba05c59be6f55e9bba73b880805999
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2014-10-10 09:36:37 +0200

    Fix major should component update flaw.

--- component.js
+++ component.js
@@ -2,26 +2,24 @@ var React = require('react');
 var Immutable = require('immutable');
 var shallowEqualImmutable = require('react-immutable-render-mixin/shallowEqualImmutable');
 
+var ShouldComponentUpdate = {
+  shouldComponentUpdate: module.exports.shouldComponentUpdate
+};
+
 module.exports = function component (mixins, render) {
   if (typeof mixins === 'function') {
     render = mixins;
     mixins = [];
   }
 
-  if (!Array.isArray(mixins)) {
-    mixins = [mixins];
-  }
-
   var Component = React.createClass({
-    mixins: mixins,
+    mixins: [ShouldComponentUpdate].concat(mixins),
 
     getInitialState: function () { return {}; },
 
     render: function () {
       return render.call(this, this.props.cursor, this.props.statics);
-    },
-
-    shouldComponentUpdate: module.exports.shouldComponentUpdate
+    }
   });
 
   return function (cursor, statics) {
@@ -34,7 +32,7 @@ module.exports.isEqualCursor = shallowEqualImmutable;
 module.exports.isEqualState = shallowEqualImmutable;
 
 function shouldComponentUpdate (nextProps, nextState) {
-  var shouldUpdate = !module.exports.isEqualCursor(this.props.cursor, nextProps.cursor) ||
+  var shouldUpdate = !module.exports.isEqualCursor(this.props.cursor.deref(), nextProps.cursor.deref()) ||
                      !module.exports.isEqualState(this.state, nextState);
   return shouldUpdate;
 }
