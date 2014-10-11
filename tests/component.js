c3fc05fd91130193e1bf1797ec2d987039917f35
Mikael Brevik <mikaelbre@gmail.com>
2014-10-11 21:43:24 +0200

    Adds more component tests

--- tests/component.js
+++ tests/component.js
@@ -31,6 +31,43 @@ describe('component', function () {
 
       ReactTestUtils.renderIntoDocument(Component());
     });
+
+    it('should be able to set a React key', function (done) {
+      var mixins = [{ componentDidMount: done, myMixin: noop }];
+      var Component = component(mixins, function () {
+        this.props.should.have.property('key');
+        this.props.key.should.equal('myKey');
+        return React.DOM.text(null, 'hello');
+      });
+
+      ReactTestUtils.renderIntoDocument(Component('myKey'));
+    });
+
+    it('should be able to set a statics', function (done) {
+      var mixins = [{ componentDidMount: done, myMixin: noop }];
+      var Component = component(mixins, function (cursor, statics) {
+        statics.should.have.property('foo');
+        statics.foo.should.equal('hello');
+        return React.DOM.text(null, 'hello');
+      });
+
+      ReactTestUtils.renderIntoDocument(Component({ }, { foo: 'hello' }));
+    });
+
+    it('should be able to override shouldComponentUpdate', function (done) {
+      var shouldUpdate = function (nextProps) {  };
+      var mixins = [{
+        componentDidMount: done,
+        shouldComponentUpdate: shouldUpdate
+      }];
+      var Component = component(mixins, function () {
+        this.shouldComponentUpdate.should.equal(shouldUpdate);
+        return React.DOM.text(null, 'hello');
+      });
+
+      ReactTestUtils.renderIntoDocument(Component({ foo: 'hello' }));
+    });
+
   });
 });
 
