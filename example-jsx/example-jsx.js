e4f947a99bbbd62bc56f970fc609888963a5e394
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2014-10-15 20:46:03 +0200

    Adds jsx-example.

---
+++ example-jsx/example-jsx.js
@@ -0,0 +1,32 @@
+/**
+ * @jsx React.DOM
+ */
+
+var React = require('react');
+    immstruct = require('immstruct'),
+
+var component = require('../');
+
+var Clock = component(function (cursors) {
+  var now = cursors.clock.get('now').toString();
+  return <p>Today is {now}</p>;
+});
+
+var structure = immstruct({ clock: { now: new Date() } });
+
+setInterval(function () {
+  structure.cursor('clock').update('now', function () {
+    return new Date();
+  });
+}, 1000);
+
+$ = document.querySelector.bind(document);
+
+render();
+structure.on('swap', render);
+
+function render () {
+  React.renderComponent(
+    <Clock clock={structure.cursor('clock')} />,
+    $('.example-jsx'));
+}
